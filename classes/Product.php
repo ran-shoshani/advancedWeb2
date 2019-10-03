30d51434f6f0d3d5713a7cfe0870a127c5ad2b0a
ran <6369@ait.nsw.edu.au>
2019-10-03 17:08:46 +1000

    error fixing

--- classes/Product.php
+++ classes/Product.php
@@ -14,16 +14,16 @@ class Product extends Database{
     public function getProducts(){
         $query = "
         SELECT 
-        @product_id := product.product_id AS product_id,
-        product.name,
-        product.description,
-        product.price,
-        product_quantity.quantity,
+        @product_id := listing.id AS product_id,
+        listing.name,
+        listing.address,
+        listing.phone_number,
+        listing.suburb,
         ( SELECT @image_id := product_image.image_id FROM product_image WHERE product_image.product_id = @product_id LIMIT 1 ) AS image_id,
         ( SELECT image_file_name FROM image WHERE image.image_id = @image_id ) AS image
-        FROM product
+        FROM listing
         INNER JOIN product_quantity
-        ON product.product_id = product_quantity.product_id
+        ON listing.id = product_quantity.product_id
         ";
 
         if( isset( $this -> category ) ){
@@ -31,16 +31,16 @@ class Product extends Database{
             " " . 
             "
             INNER JOIN
-            product_category
-            ON product_category.product_id = product.product_id
-            WHERE product_category.category_id = ?
+            listing_category
+            ON listing_category.Listing_id = listing.id
+            WHERE listing_category.Category_id = ?
             ";
         }
 
         $statement = $this -> connection -> prepare( $query );
 
-        if( isset( $this -> category ) ){
-            $statement -> bind_param( 'i', $this -> category );
+        if( isset( $this -> Category ) ){
+            $statement -> bind_param( 'i', $this -> Category );
         }
 
         if( $statement -> execute() ){
--- classes/WishList.php
+++ classes/WishList.php
@@ -1,57 +1,59 @@
-<?
+<?php
 namespace aitsydney;
-
 use aitsydney\Database;
-
 class WishList extends Database{
-    public function __constrorctor(){
-parent::__construct();
-
+  private $account_id;
+  private $wishlist_id;
+  public function __construct(){
+    parent::__construct();
+    $this -> account_id = $this -> getAccountId();
+    $this -> wishlist_id = $this -> getListId();
+  }
+  public function getAccountId(){
+    // initialise session
+    if( session_status() == PHP_SESSION_NONE ){
+      session_start();
     }
-public function getAccountId(){
- // initialise session
- if (session_status() == PHP_SESSION_NONE){
-
-    session_start();
- }
- return $account_id;
-}
-public function getlistID(){
+    $account_id = ( isset($_SESSION['auth'])) ? $_SESSION['auth'] : null ;
+    return $account_id;
+  }
+  
+  public function getListId(){
     $account_id = $this -> getAccountId();
-    //the user is logged in
-    if($account_id){
-        $find_Query = "SELECT wichlist_id FROM wishlist WHERE account_id = UNHEX( ? ) ";
-        $statement = $this -> connection -> prepare ( $find_query );
-        $statement -> bind_param('s', $account_id );
-         if( $statement -> execute() ){
-             $result = $statement -> get_result();
-             if( $result -> num_rows == 0 ){
-                 ///user list not in databse
-                 //craete the list
-                 $list_id = $this  -> createNewList ( $account_id );
-                 return $list_id;
-             }
-             else{
-                 $row = $result -> fetch_assoc();
-                 return $row['wishlist_id'];
-             }
-         }                                                       
+    // the user is logged in
+    if( $account_id ){
+      // find the user's list
+      $find_query = "SELECT wishlist_id FROM wishlist WHERE account_id = UNHEX( ? ) ";
+      $statement = $this -> connection -> prepare( $find_query );
+      $statement -> bind_param( 's', $account_id );
+      if( $statement -> execute() ){
+        $result = $statement -> get_result();
+        if( $result -> num_rows == 0 ){
+          //user list not in database
+          // create the list
+          $list_id = $this -> createNewList( $account_id );
+          return $list_id;
+        }
+        else{
+          $row = $result -> fetch_assoc();
+          return $row['wishlist_id'];
+        }
+      }
     }
-    //the user is not logget in
+    // the user is not logged in
     {
-        return null;
+      return null;
     }
-}
-private function createNewList( $account_id ){
-    $create_query = "INSERT INTO wishlist (account_id) VALUES ( UNHEX(?) ) ";
-    $statement = $this -> connection -> prepare ( $create_query );
-    $statement -> bind_param ( 's', $account_id );
-    $statement ->execute();
+  }
+  private function createNewList( $account_id ){
+    $create_query = "INSERT INTO wishlist (account_id) VALUES( UNHEX(?) )";
+    $statement = $this -> connection -> prepare( $create_query );
+    $statement -> bind_param('s', $account_id );
+    $statement -> execute();
     $wishlist_id = $this -> connection -> insert_id;
     return $wishlist_id;
-
-    }
-    //public function
+  }
+  public function setItem( $product_id ){
+  }
 }
-
 ?>
